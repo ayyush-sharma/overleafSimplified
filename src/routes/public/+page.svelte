3ece957e8a4cf061f68d9ac516d0f351e2f2f4d8
Ayush Sharma <ayush.sharma954088@gmail.com>
2024-09-13 16:35:45 +0530

    small padding changes

--- src/routes/public/+page.svelte
+++ src/routes/public/+page.svelte
@@ -207,8 +207,8 @@
 
 
 <!-- beautiful multi step form  -->
-<div class="min-h-screen  pt-20 sm:pt-0 p-6 bg-gray-100 flex items-center justify-center">
-	<div class="container max-w-screen-lg mx-auto">
+<div class="min-h-screen p-6 bg-gray-100 flex ">
+	<div class="container max-w-screen-lg mt-20 mx-auto">
 		<div
         >
 			<div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
